2630000872f93362f657ce9d64c933d55f37c635
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-23 05:50:15 +0700

    Add files via upload

---
+++ tugas26.js
@@ -0,0 +1,11 @@
+var angka = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]
+
+angka.forEach(function (element){
+	if (isFinite(angka===true)) { 
+	console.log("Angka :", element, "Infiniti")
+	}else {
+	console.log("Angka :", element, "Not Infiniti")
+	}
+})
+
+
